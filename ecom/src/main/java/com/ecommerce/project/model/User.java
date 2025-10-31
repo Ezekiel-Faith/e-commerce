783e35262fe3d02ab306ee951277bac03199d042
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-10-31 13:35:00 +0100

    added validation message

--- ecom/src/main/java/com/ecommerce/project/model/User.java
+++ ecom/src/main/java/com/ecommerce/project/model/User.java
@@ -28,18 +28,18 @@ public class User {
     private Long userId;
 
     @NotBlank
-    @Size(min = 5, max = 20)
+    @Size(min = 5, max = 20, message = "Username must be at least 5 characters")
     @Column(name = "username")
     private String username;
 
     @NotBlank
-    @Size(min = 5, max = 50)
+    @Size(min = 5, max = 50, message = "Email must be at least 5 characters")
     @Email
     @Column(name = "email")
     private String email;
 
     @NotBlank
-    @Size(min = 6, max = 120)
+    @Size(min = 6, max = 120, message = "password must be at least 6 characters")
     @Column(name = "password")
     private String password;
 
