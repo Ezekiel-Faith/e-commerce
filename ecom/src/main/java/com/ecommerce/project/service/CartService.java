13778b48c92f6e6f03fe86d5b0b3668bf47aa9a9
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-11-11 16:15:47 +0100

    fetching all carts

--- ecom/src/main/java/com/ecommerce/project/service/CartService.java
+++ ecom/src/main/java/com/ecommerce/project/service/CartService.java
@@ -2,7 +2,11 @@ package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
 
     CartDTO addProductToCart(Long productid, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }
