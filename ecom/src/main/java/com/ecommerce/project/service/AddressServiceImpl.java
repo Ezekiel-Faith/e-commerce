7758cf4b04daa46a8c9003f10747acd4774a8899
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2026-02-02 12:06:17 +0100

    getting addresses

--- ecom/src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ ecom/src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -31,6 +32,13 @@ public class AddressServiceImpl implements AddressService {
         Address savedAddress = addressRepository.save(address);
 
         return modelMapper.map(savedAddress, AddressDTO.class);
+    }
 
+    @Override
+    public List<AddressDTO> getAddresses() {
+        List<Address> addressList = addressRepository.findAll();
+        return addressList.stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .toList();
     }
 }
